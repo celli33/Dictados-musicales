0632c201dfa0c2f39ba850c6f87a368f15863a4e
celli33 <misachiva_@hotmail.com>
2016-11-19 10:15:13 -0600

    Add files via upload

--- DictadoFacil.java
+++ DictadoFacil.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 //clase que contiene dos atributos las notas posibles del dictado facil
 //un array de notas que contiene las notas posibles
@@ -7,10 +8,14 @@ public class DictadoFacil {
 	//
 	Nota b4,c5,d5,e5,f5,g5,a5,b5,c6,d6,e6,f6,g6;
 	Nota[] notasPosibles;
+	public final static int   numNotasposibles=13;
+	//lista que guarda el dictado en String
+	ArrayList<String> dictadoString;
 	public DictadoFacil()
 	{
-		//inicialicamos el array
-		notasPosibles= new Nota[13];
+		//inicialicamos el array ahora inicialicamos el array con una variable final
+		//para así sea fácil modificar el número de elementos posibles en el dictado
+		notasPosibles= new Nota[ numNotasposibles];
 		//al array de notas le agregamos la nota inicializada con el nombre del nota
 		notasPosibles[0]=b4=new Nota("b4");
 		notasPosibles[1]=c5=new Nota("c5");
@@ -26,12 +31,16 @@ public class DictadoFacil {
 		notasPosibles[11]=f6= new Nota("f6");
 		notasPosibles[12]=g6= new Nota("g6");
 
+		//inicializamos el arraylist donde guardaremos el nombre de las notas para relacionarlos 
+		//con el archivo raw/"nombre de la nota" que contiene el sonido
+		dictadoString= new ArrayList<String>();
 		
 	}
 	
 	
 	public ArrayList<String> generaDictado(int numNotas)
 	{
+		ArrayList<Nota> dictado= new ArrayList<Nota>();
 		
 		//llamamos el metodo notasPosibles de la nota y le agregamos las notas posibles
 		b4.notasPosibles(c5);
@@ -48,10 +57,37 @@ public class DictadoFacil {
 		f6.notasPosibles(e6,g6);
 		g6.notasPosibles(f6);
 		
+		//generamos la nota inicial del dictado aleatoramiente
+		dictado.add(this.generaNotaInicial());
+		System.out.println("nota inicial "+ dictado.get(0).getName());
+		dictadoString.add(dictado.get(0).getName());
+		int aux=1;
+		while(dictado.size()<20)
+		{
+			dictado.add(dictado.get(aux-1).eligeNotaSiguiente());
+			dictadoString.add(dictado.get(aux).getName());
+			//System.out.println("agregada nota: "+ dictado.get(aux).getName());
+			aux++;
+			
+		}
 		
 		//por ahora no regresemas ningun dictado
-		return(null);
+		return(dictadoString);
 	}
+	
+	//metodo que genera la nota inicial del dictado
+	public Nota generaNotaInicial()
+	{
+		//definimos un random para el numero aleatorio
+		Random rnd= new Random();
+		//casteamos el aleatorio y lo ponemos en un rango de 0 al numero de notas Posibles
+		int aux=(int) (rnd.nextDouble()*this.numNotasposibles);
+		
+		//retornamos la primera nota
+		return(notasPosibles[aux]);
+	}
+
+	
 	
 	
 
--- TestFacil.java
+++ TestFacil.java
@@ -11,8 +11,19 @@ public class TestFacil {
 		DictadoFacil df= new DictadoFacil();
 		//creamos un arrayList de string que guardará la sucesion de notas del dictado
 		//mandamos el número de notas que contendrá el dictado
-		ArrayList<String> dictado=df.generaDictado(20);
 		
+		ArrayList<String> dictado;
+		
+		
+		dictado=df.generaDictado(20);
+		int index=0;
+		System.out.println("");
+		while(index<dictado.size())
+		{
+			
+			System.out.print(dictado.get(index)+",");
+			index++;
+		}
 		
 		
 	}
